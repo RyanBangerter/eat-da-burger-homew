28120b1a3e230f8820b319e8b8b875a844a9c240
Ryan <bangerterryan350@gmail.com>
2018-05-15 16:26:16 -0600

    change

--- db/seeds.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO burgers (burger_name) VALUES ('Small Mac');
-INSERT INTO burgers (burger_name) VALUES ('Medium Mac');
-INSERT INTO burgers (burger_name) VALUES ('Giant Mac');
-INSERT INTO burgers (burger_name) VALUES ('xsmal Mac');
-INSERT INTO burgers (burger_name) VALUES ('Huge Mac');
