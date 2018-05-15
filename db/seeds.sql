5588f4891c66de46eb8e2e109217235b7142e15e
RyanBangerter <34459891+RyanBangerter@users.noreply.github.com>
2018-05-15 13:47:02 -0600

    Add files via upload

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name) VALUES ('Small Mac');
+INSERT INTO burgers (burger_name) VALUES ('Medium Mac');
+INSERT INTO burgers (burger_name) VALUES ('Giant Mac');
+INSERT INTO burgers (burger_name) VALUES ('xsmal Mac');
+INSERT INTO burgers (burger_name) VALUES ('Huge Mac');
